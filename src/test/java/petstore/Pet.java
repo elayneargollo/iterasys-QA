e56b91c828a8db118b18f2746c155f239f61ae85
Elayne Argollo <elayne.natalia@outlook.com>
2021-08-17 15:11:25 -0300

    update test

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,15 +1,19 @@
 package petstore;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.TestInstance;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static io.restassured.RestAssured.given;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static io.restassured.RestAssured.given;
-
 public class Pet {
     final String URI_SWAGGER = "https://petstore.swagger.io/v2/pet";
     final String PATH_PET_INCLUSAO = "db/petEntity.json";
+    final String PATH_PET_ATUALIZACAO = "db/petEntityUpdate.json";
     final String CONTENT_TYPE = "application/json";
 
     public String lerJson(String caminhoJson) throws IOException
@@ -30,6 +34,44 @@ public class Pet {
                 .post(URI_SWAGGER)
         .then()
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("Nino"))
+                .body("status", is("available"))
+                .body("category.name",is("dog"))
+                .body("tags.name",contains("sta"));
+    }
+
+    @Test
+    public void consultarPet()
+    {
+        String IdEntity = "2018116017";
+
+        given()
+                .contentType(CONTENT_TYPE)
+                .log().all()
+        .when()
+                .get(URI_SWAGGER.concat("/").concat(IdEntity))
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Nino"))
+                .body("status", is("available"));
+    }
+
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson(PATH_PET_ATUALIZACAO);
+
+        given()
+                .contentType(CONTENT_TYPE)
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(URI_SWAGGER)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Nino"))
+                .body("status", is("sold"));
     }
 }
